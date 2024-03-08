9d972ba0a90788b95de5e302ddeb8fa025a95aa0
Tetiana Poletaieva <ptupob@gmail.com>
2024-03-08 08:23:18 +0200

    change image

--- src/drug/drug.controller.ts
+++ src/drug/drug.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
 import { DrugService } from './drug.service';
 import { ReturnDrugDto } from './dto/drug.dto';
 
@@ -9,5 +9,14 @@ export class DrugController {
   @Get(':idCompany')
   async findByCompany(@Param('idCompany') idCompany): Promise<ReturnDrugDto[]> {
     return await this.drugService.findByCompany(Number(idCompany));
+  }
+
+  @Get('image/:imagename')
+  getImage(@Param('imagename') image: string, @Res() res) {
+    const response = res.sendFile(image, { root: './uploads' });
+    return {
+      status: HttpStatus.OK,
+      data: response,
+    };
   }
 }
